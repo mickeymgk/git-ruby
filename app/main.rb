1bc4e274cc9f6483810396b68b3ddcfe43cfd2f7
mickeymgk <35106419+mickeymgk@users.noreply.github.com>
2024-02-13 23:27:12 +0300

    pass stage 2

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,6 @@
+require zlib
 # You can use print statements as follows for debugging, they'll be visible when running tests.
-puts "Logs from your program will appear here!"
+# puts "Logs from your program will appear here!"
 
 # Uncomment this block to pass the first stage
 
@@ -11,6 +12,13 @@ when "init"
   Dir.mkdir(".git/refs")
   File.write(".git/HEAD", "ref: refs/heads/master\n")
   puts "Initialized git directory"
+when "cat-file"
+  object_hash = ARGV[2]
+  path = ".git/objects/#{object_hash[0,2]}/#{object_hash[2,38]}"
+  compressed = File.read(path)
+  uncompressed = Zlib::Inflate.inflate(compressed)
+  header, content = uncompressed.split("\0")
+  print content
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
